112e6d093117d97671bf51f3ca188ac74db3f5b1
leopold-pfeiffer <leopold.pfeiffer@gmx.de>
2020-07-03 16:36:53 +0200

    Fixed a nasty bug

--- lib/Paper/Settingstests.py
+++ lib/Paper/Settingstests.py
@@ -3,39 +3,37 @@ import time
 from tqdm import tqdm
 from model.SupplyChain2 import runSC
 import pandas as pd
+from temp.regexparser import getlists, getlists2
 
 
 def simulate(name):
+
+    demand, ilt = getlists2('temp/Setting2.txt')
+    if name == 'A':
+        ilt = [[x, x, x, x, x] for x in ilt]
+
     t = time.time()
     tscc_list = []
 
     for i in tqdm([*range(n_it)]):
-        if name == "A":
-            arg = {
-                'hcs': np.array([4, 3, 2, 1]),
-                'scs': np.array([8, 6, 4, 2]),
-                'rlt': np.array([2, 3, 4, 5]),
-                'ilt': np.random.randint(0, 5, 4),
-                'RMSilt': np.random.randint(0, 5, 1)[0]
+        arg = {
+            'hcs': np.array([4, 3, 2, 1]),
+            'scs': np.array([8, 6, 4, 2]),
+            'rlt': np.array([2, 3, 4, 5]),
+            'ilt': np.random.randint(0, 5, 4),
+            'RMSilt': np.random.randint(0, 5, 1)[0]
             }
-        elif name == "B":
-            arg = {
-                'hcs': np.array([4, 3, 2, 1]),
-                'scs': np.array([8, 6, 4, 2]),
-                'rlt': np.random.randint(0, 5, 4),
-                'ilt': np.random.randint(0, 5, 4),
-                'RMSilt': np.random.randint(0, 5, 1)[0]
-            }
-        tscc = runSC(chromosome=[100, 100, 100, 100], args=arg, name=name)
+
+        tscc = runSC(chromosome=[100, 100, 100, 100], args=arg, name=name, demand=demand, ilt_list=ilt)
         tscc_list.append(tscc)
 
     tscc_df = pd.DataFrame(tscc_list)
-    tscc_df.to_csv(name + '2.csv')
+    tscc_df.to_csv('out.csv')
     print('\n', name, 'Done. Time:', time.time() - t, '\n\n')
 
 
 if __name__ == "__main__":
-    n_it = 10000  # 10000
+    n_it = 1
 
-    simulate(name='A')
+    # simulate(name='A')
     simulate(name='B')
--- lib/Paper/model/SupplyChain2.py
+++ lib/Paper/model/SupplyChain2.py
@@ -8,7 +8,7 @@ class Agent:
 
     def __init__(self, no, basestock, ilt, rlt, RMSilt, hcs, scs):
         self.no = no
-        self.T = 1200
+        self.T = 100
         self.basestock = basestock
         self.ilt = ilt
         self.RMSilt = RMSilt
@@ -26,9 +26,9 @@ class Agent:
 
 class SupplyChain:
 
-    def __init__(self, agents, demand, name):
+    def __init__(self, agents, demand, name, ilt_list):
         self.agents = agents
-        self.T = 1200
+        self.T = 100
         self.demand = demand
         self.scc = [0] * self.T
         self.tscc = 0
@@ -36,28 +36,51 @@ class SupplyChain:
         self.s_costs = []
         self.inventory = []
         self.name = name
+        self.ilt_list = ilt_list
 
     def simulate(self):
+        def rec(t):
+            return [x.receive[t] for x in self.agents]
+
+        bl = []
+
         for t in range(0, self.T):
 
-            # Change Lead Times every period
-            if self.name == 'A':
-                ilt = np.random.randint(0, 5, 4)
-                RMSilt = np.random.randint(0, 5, 1)[0]
+            ilt = self.ilt_list[t][0:4]
+            RMSilt = self.ilt_list[t][4]
 
-                for i, agent in enumerate(self.agents):
-                    agent.ilt = ilt[i]
-                    agent.RMSilt = RMSilt
+            for i, agent in enumerate(self.agents):
+                agent.ilt = ilt[i]
+                agent.RMSilt = RMSilt
 
-            if self.name == 'B:':
-                rlt = np.random.randint(0, 5, 4)
-                ilt = np.random.randint(0, 5, 4)
-                RMSilt = np.random.randint(0, 5, 1)[0]
+            """
+            ohi = [x.onHandInventory-x.backlog for x in self.agents]
 
-                for i, agent in enumerate(self.agents):
-                    agent.ilt = ilt[i]
-                    agent.RMSilt = RMSilt
-                    agent.rlt = rlt[i]
+            print('\nxxxxxxxxxxxx start xxxxxxxxxxx')
+
+            print(f't: {t}')
+            print(f'{ohi[0]}[{rec(t)[0]}] {ohi[1]}[{rec(t)[1]}] {ohi[2]}[{rec(t)[2]}] {ohi[3]}[{rec(t)[3]}]')
+            print(f'{rec(t+1)[0]}[] {rec(t+1)[1]}[] {rec(t+1)[2]}[] {rec(t+1)[3]}[]')
+            print(f'{rec(t+2)[0]}[] {rec(t+2)[1]}[] {rec(t+2)[2]}[] {rec(t+2)[3]}[]')
+            print(f'{rec(t+3)[0]}[] {rec(t+3)[1]}[] {rec(t+3)[2]}[] {rec(t+3)[3]}[]')
+            print(f'{rec(t+4)[0]}[] {rec(t+4)[1]}[] {rec(t+4)[2]}[] {rec(t+4)[3]}[]')
+            print(f'Demand: {self.demand[t]}')
+            print(f'ILT: {self.ilt_list[t][i]}')
+            print('xxxxxxxxxxxx  end  xxxxxxxxxxx\n')
+            int(0)
+            """
+
+            print(f"\nt. {t}-----")
+            print('net', [x.onHandInventory - x.backlog for x in self.agents])
+            print('ohi', [x.onHandInventory for x in self.agents])
+            print('rec', [x.receive[t] for x in self.agents])
+            print('ord', [x.order[t] for x in self.agents])
+            print('ooi', [x.onOrderInventory for x in self.agents])
+            print('blo', [x.backlog for x in self.agents])
+            print("dem", self.demand[t])
+            print("ilt", self.ilt_list[t])
+            print("\n")
+            bl.append(self.agents[0].backlog)
 
             # update the order info from
             for i, agent in enumerate(self.agents):
@@ -104,7 +127,7 @@ class SupplyChain:
 
                 # orders are received in t + ILT
                 if agent.no != 3:
-                    upstream.order[t + upstream.ilt] = orderQuantity
+                    upstream.order[t + upstream.ilt] += orderQuantity
                 else:
                     # supplier receives order from infinite source after rlt + ILT
                     agent.receive[t + agent.rlt + agent.RMSilt] += orderQuantity
@@ -123,7 +146,6 @@ class SupplyChain:
 
         self.tscc = np.array(self.scc[0:]).cumsum()[-1]
 
-
 def returnTSCC(chromosome):
     """Used for testing."""
     goal = [12, 13, 4, 17]
@@ -136,14 +158,15 @@ def runSC(chromosome, args, **kwargs):
     scs = args['scs']
     ilt = args['ilt']
     RMSilt = args['RMSilt']
-    demand = kwargs.get('demand', np.random.randint(20, 61, 1200))
+    demand = kwargs.get('demand', np.random.randint(20, 61, 100))
     agents = []
     name = kwargs.get('name')
+    ilt_list = kwargs.get('ilt_list', np.random.randint(0, 5, 100))
 
     for i, chrom in enumerate(chromosome):
         agents.append(Agent(no=i, basestock=chrom, rlt=rlt[i], hcs=hcs[i], RMSilt=RMSilt, scs=scs[i], ilt=ilt[i]))
 
-    S = SupplyChain(agents=agents, demand=demand, name=name)
+    S = SupplyChain(agents=agents, demand=demand, name=name, ilt_list=ilt_list)
     S.simulate()
 
     return S.tscc
