212fd4e9ebf18d8cb9935b55753ed1213e2bf8d7
leopold-pfeiffer <leopold.pfeiffer@gmx.de>
2020-07-10 16:44:50 +0200

    Refactoring

--- lib/Paper/simulations/Simulate_paper.py
+++ lib/Paper/simulations/Simulate_paper.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from GenAlg_paper import GenAlg
+from model.GenAlg_paper import GenAlg
 from model.SupplyChain_paper import runSC
 from model.SCsettings_paper import a1, a2, a3, b1, b2, b3, randomArgs, demandSample, Output
 import time
